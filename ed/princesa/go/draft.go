6f36811e3a5993657eed141a520c8cefba4a9c2c
Ryan <ryanlbbbb@gmail.com>
2026-03-12 11:29:55 -0300

    continuei princesa

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -3,16 +3,49 @@ package main
 import (
 	"fmt"
 )
+
 func main() {
-    var N, E int
-    fmt.Scan(&N, &E)
+	var N, espada int
+	fmt.Scan(&N, &espada)
 
-    fila := make([]int, N)
+	vaiMorre := espada
+	fila := make([]int, N)
 
-    for i := range fila {
-        fila[i] = i + 1
-    }
+	for i := range fila {
+		fila[i] = i + 1
+	}
 
-    
-    fmt.Println(fila)
+	mortos := 0
+
+	for mortos < N - 2 {
+		fmt.Print("[ ")
+
+        // primeira iteracao, imprime todos
+		if mortos == 0 {
+			for i, v := range fila {
+				fmt.Print(v)
+
+				if espada == i+1 {
+					fmt.Print("> ")
+				}
+			}
+            fmt.Println(" ]")
+            fila[vaiMorre] = 0
+            mortos++
+		}
+
+		for i, v := range fila {
+			if v != 0 {
+				fmt.Print(v)
+			}
+
+			if espada == i+1 {
+				fmt.Print("> ")
+			}
+
+			fila[vaiMorre] = 0
+		}
+		mortos++
+		fmt.Println(" ]")
+	}
 }
