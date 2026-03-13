32f7739e768492bc818112ebf7822a0ee1d2f53a
Ryan <ryanlbbbb@gmail.com>
2026-03-13 11:36:44 -0300

    continuei apagando

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -1,11 +1,14 @@
 package main
 import "fmt"
 func main() {
-	var fila1, fila2 int
-	fmt.Scan(&fila1)
-	for _, v := range fila1 {
+	var nF1, nF2 int
+	fmt.Scan(&nF1)
+
+	fila1 := make([]int, nF1)
+	
+	for i := range fila1 {
 		var valor int
 		fmt.Scan(&valor)
-		v = valor
+		fila1[i] = valor 
 	}
 }
