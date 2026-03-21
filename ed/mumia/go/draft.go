128445074f4f2f5220b77d1ead55762557b90a8c
Ryan <ryanlbbbb@gmail.com>
2026-03-21 15:46:01 -0300

    pintando e mumia

--- ed/mumia/go/draft.go
+++ ed/mumia/go/draft.go
@@ -1,5 +1,20 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var idade int
+    var nome string
+    fmt.Scan(&nome, &idade)
+
+    switch {
+    case idade < 12:
+        fmt.Println(nome,"eh crianca")
+    case idade < 18:
+        fmt.Println(nome, "eh jovem")
+    case idade < 65:
+        fmt.Println(nome, "eh adulto")
+    case idade < 100:
+        fmt.Println(nome, "eh idoso")
+    default:
+        fmt.Println(nome, "eh mumia")
+    }
 }
--- ed/pintando/go/draft.go
+++ ed/pintando/go/draft.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-    var a, b, c float64
-    fmt.Scan(&a, &b, &c)
-    p :=  (a + b + c) / 2 
-    fmt.Println(math.Sqrt(p * (p - a) * (p - b) * (p - c)))
+	var a, b, c float64
+	fmt.Scan(&a, &b, &c)
+	var p float64 = (a + b + c) / 2
+	var area float64 = math.Sqrt(p * (p - a) * (p - b) * (p - c))
+
+	fmt.Printf("%.2f\n", area)
 }
