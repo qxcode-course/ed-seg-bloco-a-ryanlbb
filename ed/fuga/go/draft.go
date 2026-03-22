9d5e491194bc15d24b13399ab8272af005c92166
Ryan <ryanlbbbb@gmail.com>
2026-03-22 19:06:11 -0300

    fiz fuga

--- ed/fuga/go/draft.go
+++ ed/fuga/go/draft.go
@@ -1,5 +1,26 @@
 package main
+
 import "fmt"
+
+func fuga(H, P, F, D int) string {
+	for F != H && F != P {
+		if D == 1 {
+			F = (F + 1) % 16
+		}
+
+		if D == -1 {
+			F = (F - 1 + 16) % 16
+		}
+	}
+
+	if F == H {
+		return "S"
+	}
+	return "N"
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	var H, P, F, D int
+	fmt.Scan(&H, &P, &F, &D)
+	fmt.Println(fuga(H, P, F, D))
 }
