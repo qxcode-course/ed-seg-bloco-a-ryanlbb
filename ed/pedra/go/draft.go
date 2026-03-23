d97a47ce4ec9555970f75e4faf549a3605fc8e20
Ryan <ryanlbbbb@gmail.com>
2026-03-22 23:04:45 -0300

    fzi pedra

--- ed/pedra/go/draft.go
+++ ed/pedra/go/draft.go
@@ -1,5 +1,45 @@
 package main
+
 import "fmt"
+
+func diferencaAbs(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return (a - b) * -1
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	var n int
+	fmt.Scan(&n)
+	diferencas := make([]int, n)
+
+	for i := range n {
+		var jgd1, jgd2 int
+		fmt.Scan(&jgd1, &jgd2)
+
+		if jgd1 < 10 || jgd2 < 10 {
+			diferencas[i] = -1
+			continue
+		} else {
+			diferencas[i] = diferencaAbs(jgd1, jgd2)
+		}
+	}
+
+	menor := -1
+    menorVal := 999999999999999999
+
+	for i := range diferencas {
+		if diferencas[i] < menorVal && diferencas[i] != -1 {
+			menor = i
+		} else {
+			continue
+		}
+	}
+
+	if menor == -1 {
+		fmt.Println("sem ganhador")
+		return
+	}
+	fmt.Println(menor)
 }
