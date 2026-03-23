5ba5a8261c9dd11809c38521ab070ace44a72d7c
Ryan <ryanlbbbb@gmail.com>
2026-03-23 16:59:16 -0300

    fiz apagando

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -1,31 +1,32 @@
 package main
-import "fmt"
 
-type Pessoa struct {
-    Id  int
-    naFila bool
-}
+import (
+	"fmt"
+)
 
 func main() {
 	var nFila, nDeixouFila int
 	fmt.Scan(&nFila)
-
-	fila := make([]Pessoa, nFila)
-	
-	for i := range fila {
-		fmt.Scan(&fila[i].Id)
-		fila[i].naFila = true
+	fila := make([]int, nFila)
+	for i := range nFila {
+		var valor int
+		fmt.Scan(&valor)
+		fila[i] = valor
 	}
 
 	fmt.Scan(&nDeixouFila)
+	deixaramFila := make(map[int]bool)
 	for range nDeixouFila {
 		var valor int
 		fmt.Scan(&valor)
+		deixaramFila[valor] = true
 	}
 
-	for i := 0; i < nFila-1; i++ {
-		if fila[i].naFila == true {
-			fmt.Print(fila[i].Id, " ")
+	for i, v := range fila {
+		if deixaramFila[v] == true {
+			continue
+		} else {
+			fmt.Print(fila[i], " ")
 		}
 	}
 
