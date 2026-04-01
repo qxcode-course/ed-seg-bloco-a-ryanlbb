ee04fa8882af0b27f21cf7acd1b000091a3e519d
Ryan <ryanlbbbb@gmail.com>
2026-04-01 20:09:58 -0300

    fiz rotacoes

--- ed/rotacoes/go/draft.go
+++ ed/rotacoes/go/draft.go
@@ -1,5 +1,22 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var n, rotacoes int
+    fmt.Scan(&n, &rotacoes)
+    vet := make([]int, n)
+    rotacionado := make([]int, n)
+    for i := range n {
+        vet[i] = i + 1
+        if rotacoes > 0 {
+            rotacionado[(i + rotacoes) % n] = i + 1 
+            continue
+        }
+        rotacionado[i] = i + 1
+    }
+
+    fmt.Print("[ ")
+    for _, v := range rotacionado {
+        fmt.Print(v, " ")
+    }
+    fmt.Println("]")
 }
