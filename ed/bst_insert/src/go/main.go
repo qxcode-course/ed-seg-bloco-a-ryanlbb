9e54c5e10115bacf8dbf13e198f6c279f87b71ab
ryanlnx <ryanlbbbb@gmail.com>
2026-07-12 15:25:53 -0300

    fiz insercao arvore

---
+++ ed/bst_insert/src/go/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+func BstInsert(values []int) *Node {
+	root := &Node{Value: values[0]}
+	for i, v := range values {
+		if i == 0 {
+			continue
+		}
+		root.insert(v)
+	}
+
+	return root
+}
+
+func (n *Node) insert(v int) {
+	if n.Value == v {
+		return
+	}
+
+	if v > n.Value {
+		if n.Right == nil {
+			n.Right = &Node{Value: v}
+			return
+		} else {
+			n.Right.insert(v)
+		}
+	} else if v < n.Value {
+		if n.Left == nil {
+			n.Left = &Node{Value: v}
+			return
+		} else {
+			n.Left.insert(v)
+		}
+	}
+}
+
+// -----------------------------------------------------------------------------------
+func BShow(node *Node, history string) {
+	if node != nil && (node.Left != nil || node.Right != nil) {
+		BShow(node.Left, history+"l")
+	}
+	for i := 0; i < len(history)-1; i++ {
+		if history[i] != history[i+1] {
+			fmt.Print("│   ")
+		} else {
+			fmt.Print("    ")
+		}
+	}
+	if history != "" {
+		if history[len(history)-1] == 'l' {
+			fmt.Print("╭───")
+		} else {
+			fmt.Print("╰───")
+		}
+	}
+	if node == nil {
+		fmt.Println("#")
+		return
+	}
+	fmt.Println(node.Value)
+	if node.Left != nil || node.Right != nil {
+		BShow(node.Right, history+"r")
+	}
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	parts := strings.Split(scanner.Text(), " ")
+	values := make([]int, 0, len(parts))
+	for _, elem := range parts {
+		v, err := strconv.Atoi(elem)
+		if err == nil {
+			values = append(values, v)
+		}
+	}
+	root := BstInsert(values)
+	BShow(root, "") // Chama a função de impressão formatada
+}
